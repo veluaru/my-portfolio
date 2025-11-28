cb9244299060d786636a989146f124a5a1da8d14
Angie Castañeda <angie.castaneda@guane.com.co>
2025-11-28 18:00:43 -0500

    :lipstick: Fix in App.tsx unused code

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import { usePortfolioStore } from './store/usePortfolioStore';
 
 function App() {
@@ -22,20 +20,12 @@ function App() {
     <Router>
       <div className="min-h-screen w-full bg-background-light dark:bg-background-dark font-display text-gray-800 dark:text-gray-200 transition-colors duration-300">
         <div className="relative flex min-h-screen w-full flex-col">
-          
-          {/* Header stays visible on all pages */}
           <Header />
-          
-          <main className="flex-grow flex flex-col items-center w-full">
+          <main className="grow flex flex-col items-center w-full">
             <Routes>
-              {/* This is where we switch between views */}
               <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
             </Routes>
           </main>
-          
-          {/* Footer stays visible on all pages */}
           <Footer />
           
         </div>
