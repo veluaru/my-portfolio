f0c0d02fac7d7fd9fa00a023903124610f1a1fa6
Angie Castañeda <angie.castaneda@guane.com.co>
2025-11-28 19:03:44 -0500

    :fire: Removing unused code

--- src/sections/ProjectsGrid.tsx
+++ src/sections/ProjectsGrid.tsx
@@ -6,9 +6,6 @@ const ProjectsGrid = () => (
     <h2 className="text-[22px] font-bold leading-tight text-gray-900 dark:text-white">
       Featured Projects
     </h2>
-    <div className="flex items-center justify-end px-4 pb-6 max-w-[960px] mx-auto">
-        <a href="#" className="text-primary text-sm font-medium hover:underline">View All</a>
-    </div>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4 max-w-[960px] mx-auto">
       {projectsListData.map((project) => (
         <ProjectCard key={project.id} project={project} />
